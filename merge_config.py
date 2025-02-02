4d498878a4a9725c9ed5b76972603cae3892112b
pab0bytes <paolo.rizzi2020@libero.it>
2025-02-02 12:56:35 +0100

    Update merge_config.py

--- merge_config.py
+++ merge_config.py
@@ -2,16 +2,16 @@ import json
 from jsonmerge import merge
 
 # Carica il file config.json originale
-with open('../original_config_backup.json', 'r') as original_file:
+with open('original_config_backup.json', 'r') as original_file:
     original_config = json.load(original_file)
 
 # Carica il file config.json del fork
-with open('../fork_config_backup.json', 'r') as fork_file:
+with open('fork_config_backup.json', 'r') as fork_file:
     fork_config = json.load(fork_file)
 
 # Unisci i due file, mantenendo le personalizzazioni del fork
 updated_config = merge(original_config, fork_config)
 
 # Sovrascrivi il file config.json del fork con il nuovo contenuto
-with open('../fork_config_backup.json', 'w') as fork_file:
+with open('fork_config_backup.json', 'w') as fork_file:
     json.dump(updated_config, fork_file, indent=4)
